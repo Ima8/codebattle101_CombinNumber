b9337a97b42890160a16696cd3b4af3e930d4725
Issaret Prachitmutita <imac.monochrome@gmail.com>
2017-06-21 08:29:42 +0700

    DONE

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const Combinatorics = require('js-combinatorics');
 const async = require('async');
 
-const fileTestCase = fs.readFileSync('./testcase_data_large.txt', { encoding: 'utf8' }).toString().split('\r\n');
+const fileTestCase = fs.readFileSync('./testcase_data_small.txt', { encoding: 'utf8' }).toString().split('\r\n');
 
 const cmb = Combinatorics.bigCombination(fileTestCase, 3);
 
@@ -13,9 +13,11 @@ console.time('compute');
 function findFuckingNumber(cm) {
   return new Promise((resolve) => {
     let prev = 0;
+    let a;
+    let curr;
     async.each(cm, (_, callback) => {
-      const a = cm.next();
-      const curr = a[0] * a[1] * a[2];
+      a = cm.next();
+      curr = a[0] * a[1] * a[2];
       prev = Math.abs(curr - goal) < Math.abs(prev - goal) ? curr : prev;
       callback();
     }, (err) => {
